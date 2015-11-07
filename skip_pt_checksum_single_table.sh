5670f9616ef255b1c2a2336b7c75aaeb963bf449
mikegriffin <mikegriffin@users.noreply.github.com>
2015-11-07 12:04:23 -0600

    Update skip_pt_checksum_single_table.sh
    skip checksum for a single table when using Percona Toolkit and a specific table is lagging the slave, possibly due to a complex pk causing a slow lookup from the original table

---
+++ skip_pt_checksum_single_table.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Skip a specific table that pt-table-checksum tried to check
+
+my_slave_socket='/foo/mysql.sock'
+my_opts_file='/root/.foo.tmp.my.cnf'
+
+my_checksum_db='percona_checksum_01'c
+my_skip_table_dbname='db_name_01'
+my_skip_table_tablename='table_name_01'
+
+while true; do
+mysql --defaults-file="${my_opts_file}" -A -S "${my_slave_socket}" -ss <<< 
+                                                                           "SELECT CONCAT('KILL ',ID,'; KILL ',ID,'; SELECT SLEEP(1); STOP SLAVE SQL_THREAD; SET GLOBAL SQL_SLAVE_SKIP_COUNTER=1; SELECT @@SQL_SLAVE_SKIP_COUNTER; START SLAVE;')
+                                                                            FROM information_schema.PROCESSLIST 
+                                                                            WHERE USER='system user' AND INFO LIKE 'REPLACE INTO \`"${my_checksum_db}"\`.\`checksum\`%$"{my_skip_table_dbname}"%$"{my_skip_table_tablename}"%'" |
+mysql --defaults-file="${my_opts_file}" -A -S "${my_slave_socket}" -ss;
+sleep 0.5;
+done
