09c0cdd98dd4ae2bc30b4c6313949c660fd387a4
mikegriffin <mikegriffin@users.noreply.github.com>
2023-03-02 19:34:42 -0600

    innodb history list length diff
    is trx_rseg_history_len increasing or decreasing

---
+++ is_rseg.sh
@@ -0,0 +1,7 @@
+while true; do
+	rseg1=$(mysql -ss -e "select count from information_schema.innodb_metrics where name = 'trx_rseg_history_len';")
+	sleep 1
+	rseg2=$(mysql -ss -e "select count from information_schema.innodb_metrics where name = 'trx_rseg_history_len';")
+	[[ $rseg1 -gt $rseg2 ]] && diff=$(( $rseg1 - $rseg2 )) && echo "(-$diff) $rseg2"
+	[[ $rseg2 -gt $rseg1 ]] && diff=$(( $rseg2 - $rseg1 )) && echo "(+$diff) $rseg2"
+done
