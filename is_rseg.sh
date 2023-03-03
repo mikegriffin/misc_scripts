989bd4e4e56c4b3e146c391030be37f8e1334a20
mikegriffin <mikegriffin@users.noreply.github.com>
2023-03-02 20:36:13 -0600

    Update is_rseg.sh
    add fix for rseg eq old rseg

--- is_rseg.sh
+++ is_rseg.sh
@@ -4,4 +4,5 @@ while true; do
 	rseg2=$(mysql -ss -e "select count from information_schema.innodb_metrics where name = 'trx_rseg_history_len';")
 	[[ $rseg1 -gt $rseg2 ]] && diff=$(( $rseg1 - $rseg2 )) && echo "(-$diff) $rseg2"
 	[[ $rseg2 -gt $rseg1 ]] && diff=$(( $rseg2 - $rseg1 )) && echo "(+$diff) $rseg2"
+	[[ $rseg2 -eq $rseg1 ]] && diff=$(( $rseg2 - $rseg1 )) && echo "(+$diff) $rseg2"
 done
