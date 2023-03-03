2e07180f65b4d14b03c18c352aadf4a7af7ebd3b
mikegriffin <mikegriffin@users.noreply.github.com>
2023-03-02 21:36:57 -0600

    Update is_rseg.sh
    use ge intead of third test

--- is_rseg.sh
+++ is_rseg.sh
@@ -3,6 +3,5 @@ while true; do
 	sleep 1
 	rseg2=$(mysql -ss -e "select count from information_schema.innodb_metrics where name = 'trx_rseg_history_len';")
 	[[ $rseg1 -gt $rseg2 ]] && diff=$(( $rseg1 - $rseg2 )) && echo "(-$diff) $rseg2"
-	[[ $rseg2 -gt $rseg1 ]] && diff=$(( $rseg2 - $rseg1 )) && echo "(+$diff) $rseg2"
-	[[ $rseg2 -eq $rseg1 ]] && diff=$(( $rseg2 - $rseg1 )) && echo "(+$diff) $rseg2"
+	[[ $rseg2 -ge $rseg1 ]] && diff=$(( $rseg2 - $rseg1 )) && echo "(+$diff) $rseg2"
 done
