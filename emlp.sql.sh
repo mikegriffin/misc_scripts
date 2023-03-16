16b3d6568915b675db055d8211af73b4b824e379
mikegriffin <mikegriffin@users.noreply.github.com>
2023-03-16 16:42:04 -0500

    Update emlp.sql.sh
    print eta to replica synced: bytes processed, exec_master_log_pos, eta

--- emlp.sql.sh
+++ emlp.sql.sh
@@ -1,10 +1,16 @@
 seconds=10
+socket=/mnt/cbs/mariadb/mysql.sock
 
 while true; do
-	emlp1=$(mysql -ss -e "show slave status\G" | awk '/Exec_Master_Log_Pos/ {print $2}')
+	emlp1=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Exec_Master_Log_Pos/ {print $2}')
 	sleep $seconds
-	emlp2=$(mysql -ss -e "show slave status\G" | awk '/Exec_Master_Log_Pos/ {print $2}')
-	[[ $emlp1 -gt $emlp2 ]] && diff=$(( $emlp1 - $emlp2 )) && echo "$(( $diff/$seconds )) (-$diff) $emlp2"
-	[[ $emlp2 -gt $emlp1 ]] && diff=$(( $emlp2 - $emlp1 )) && echo "$(( $diff/$seconds )) (+$diff) $emlp2"
-	[[ $emlp2 -eq $emlp1 ]] && diff=$(( $emlp2 - $emlp1 )) && echo "0 (+$diff) $emlp2"
+	emlp2=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Exec_Master_Log_Pos/ {print $2}')
+	rls=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Relay_Log_Space/ {print $2}')
+	#[[ $emlp1 -gt $emlp2 ]] && diff=$(( $emlp1 - $emlp2 )) &&
+	  #echo "$(( $diff/$seconds )) $emlp2 eta: $(echo "scale=2; ($rls - $emlp2)/($diff/$seconds)/3600" | bc -lq) hours"
+	[[ $emlp1 -gt $emlp2 ]] && echo "recalculating"
+	[[ $emlp2 -gt $emlp1 ]] && diff=$(( $emlp2 - $emlp1 )) &&
+	  echo "$(( $diff/$seconds )) $emlp2 eta: $(echo "scale=2; ($rls - $emlp2)/($diff/$seconds)/3600" | bc -lq) hours"
+	[[ $emlp2 -eq $emlp1 ]] && diff=$(( $emlp2 - $emlp1 )) &&
+	  echo "0 $emlp2"
 done
