d5a655919529b8eca12a1e0d2032eb1be6048c2f
mikegriffin <mikegriffin@users.noreply.github.com>
2023-03-29 22:03:24 -0500

    Update emlp.sql.sh
    cosmetic change

--- emlp.sql.sh
+++ emlp.sql.sh
@@ -4,7 +4,6 @@ socket=/mnt/test/mysqldata/mysql.sock
 echo $socket
 counter=0
 while true; do
-	date
 	emlp1=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Exec_Master_Log_Pos/ {print $2}')
 	sleep $seconds
 	rmlf=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Relay_Master_Log_File/ {print $2}')
@@ -12,8 +11,9 @@ while true; do
 	rls=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Relay_Log_Space/ {print $2}')
 	mrls=$(mysql -S $socket -ss -e "select @@max_relay_log_size")
 	sbm=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Seconds_Behind_Master/ {print $2}')
-	[[ $emlp1 -gt $emlp2 ]] && echo "recalculating"
 	[[ $counter -eq 0 ]] && echo -e "\ndiff Exec_Master_Log_Pos"
+	date
+	[[ $emlp1 -gt $emlp2 ]] && echo "recalculating"
 	[[ $emlp2 -gt $emlp1 ]] && diff=$(( $emlp2 - $emlp1 )) &&
 	  echo -n "$(( $diff/$seconds )) $rmlf $emlp2 Seconds_Behind_Master: $sbm eta: $(echo "scale=2; ($rls - $emlp2)/($diff/$seconds)/3600" | bc -lq) hours" &&
 	  [[ $(( $mrls * 2 ))  -gt $rls ]] && echo " (eta possibly too large)" || echo
