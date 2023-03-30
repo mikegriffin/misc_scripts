d8a5fb2456ed415a4ea81f10d47342e253a3c361
mikegriffin <mikegriffin@users.noreply.github.com>
2023-03-29 21:45:42 -0500

    Update emlp.sql.sh
    additional info / fixes

--- emlp.sql.sh
+++ emlp.sql.sh
@@ -1,16 +1,23 @@
 seconds=10
-socket=/mnt/cbs/mariadb/mysql.sock
+socket=/mnt/test/mysqldata/mysql.sock
 
+echo $socket
+counter=0
 while true; do
+	date
 	emlp1=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Exec_Master_Log_Pos/ {print $2}')
 	sleep $seconds
+	rmlf=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Relay_Master_Log_File/ {print $2}')
 	emlp2=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Exec_Master_Log_Pos/ {print $2}')
 	rls=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Relay_Log_Space/ {print $2}')
-	#[[ $emlp1 -gt $emlp2 ]] && diff=$(( $emlp1 - $emlp2 )) &&
-	  #echo "$(( $diff/$seconds )) $emlp2 eta: $(echo "scale=2; ($rls - $emlp2)/($diff/$seconds)/3600" | bc -lq) hours"
+	mrls=$(mysql -S $socket -ss -e "select @@max_relay_log_size")
+	sbm=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Seconds_Behind_Master/ {print $2}')
 	[[ $emlp1 -gt $emlp2 ]] && echo "recalculating"
+	[[ $counter -eq 0 ]] && echo -e "\ndiff Exec_Master_Log_Pos"
 	[[ $emlp2 -gt $emlp1 ]] && diff=$(( $emlp2 - $emlp1 )) &&
-	  echo "$(( $diff/$seconds )) $emlp2 eta: $(echo "scale=2; ($rls - $emlp2)/($diff/$seconds)/3600" | bc -lq) hours"
+	  echo -n "$(( $diff/$seconds )) $rmlf $emlp2 Seconds_Behind_Master: $sbm eta: $(echo "scale=2; ($rls - $emlp2)/($diff/$seconds)/3600" | bc -lq) hours" &&
+	  [[ $(( $mrls * 2 ))  -gt $rls ]] && echo " (eta possibly too large)" || echo
 	[[ $emlp2 -eq $emlp1 ]] && diff=$(( $emlp2 - $emlp1 )) &&
-	  echo "0 $emlp2"
+	  echo "0 $rmlf $emlp2"
+	counter=$((counter+1)) && [[ $counter -gt $seconds && $counter -gt 30 ]] && counter=0
 done
