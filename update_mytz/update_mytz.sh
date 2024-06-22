ec465a242af508c8bc3acaa8f432be064ab487a6
mikegriffin <mikegriffin@users.noreply.github.com>
2024-06-21 20:48:26 -0500

    Set example cmd to use --skip-write-binlog
    It might be the case that this service runs on both sources and replicas. Added note that the change takes effect on next restart. Unfortunately, in cases where the db software was also updated by dnf, it probably restarted prior to the tzdata package being updated.

--- update_mytz/update_mytz.sh
+++ update_mytz/update_mytz.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# note, current mysql/mariadb docs indicate that a restart of daemon will be required to take effect
+
 tzdir=/usr/share/zoneinfo/
-cmd="/usr/bin/mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql"
+cmd="/usr/bin/mysql_tzinfo_to_sql --skip-write-binlog /usr/share/zoneinfo | mysql -u root mysql"
 
 while true; do
 	/usr/bin/inotifywait \
