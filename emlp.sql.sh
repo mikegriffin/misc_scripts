dd111e8c4f634aea671c10ba2eb391fbaaed56a4
mikegriffin <mikegriffin@users.noreply.github.com>
2025-05-07 03:26:09 +0000

    Improve mysql replication catch up logging

--- emlp.sql.sh
+++ emlp.sql.sh
@@ -1,25 +1,37 @@
-seconds=5
+# user configuration
+seconds=1 # increase this if output is too bouncy
 socket=/mnt/data/mysqldata/mysql.sock
 
-echo $socket
+# internal variables
 counter=0
-sbm=0
+output_description="binlog, offset, bytes/s, sbm, sbm_delta, eta, error"
+echo "Using: $socket"
 while true; do
 	emlp1=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Exec_Master_Log_Pos/ {print $2}')
 	sleep $seconds
 	rmlf=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Relay_Master_Log_File/ {print $2}')
 	emlp2=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Exec_Master_Log_Pos/ {print $2}')
+	[[ $counter -eq 0 ]] && old_rls=$rls
+	old_rls=${old_rls:=0}
 	rls=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Relay_Log_Space/ {print $2}')
 	mrls=$(mysql -S $socket -ss -e "select @@max_relay_log_size")
+	sbm=${sbm:=0}
         old_sbm=$sbm
 	sbm=$(mysql -S $socket -ss -e "show slave status\G" | awk '/Seconds_Behind_Master/ {print $2}')
-	[[ $counter -eq 0 ]] && echo -e "\nprocessed/s, binlog, offset, sbm, sbm_delta, eta, info"
+	[[ $counter -eq 0 ]] && [[ $old_rls -gt 0 && $rls -gt $old_rls && $(echo "scale=2; $rls / $mrls > 1" | bc -lq) -eq 1 &&
+					$(echo "scale=2; (($rls-$old_rls))>($mrls/100)" | bc -lq) -eq 1 ]] &&
+			echo "##############" &&
+			echo "Relay log space grew to $rls - eta likely to climb due to Replica_IO backlog" &&
+			echo "##############" &&
+			echo
+	[[ $counter -eq 0 ]] && echo -e "\n$output_description" && counter=1 && continue
 	date
-	[[ $emlp1 -gt $emlp2 ]] && echo "recalculating"
+	[[ $emlp1 -gt $emlp2 ]] && echo "recalculating since we switched to next log during check" && echo -ne "\n$output_description"
 	[[ $emlp2 -gt $emlp1 ]] && diff=$(( $emlp2 - $emlp1 )) &&
-	  echo -n "$(( $diff/$seconds )) $rmlf $emlp2 Seconds_Behind_Master: $sbm (change: $(echo "scale=2; $old_sbm - $sbm" | bc -lq)) eta: $(echo "scale=2; ($rls - $emlp2)/($diff/$seconds)/3600" | bc -lq) hours" &&
-	  [[ $(( $mrls * 2 ))  -gt $rls ]] && echo " (eta probably wrong due to Replica_IO backlog - sleeping 5 extra seconds)" && sleep 5 || echo
+			echo -n "$rmlf $emlp2 $(( $diff/$seconds )) Seconds_Behind_Master: $sbm (advanced $(echo "scale=2; $old_sbm - $sbm" | bc -lq)"
+			echo -n " seconds in $seconds seconds) eta: $(echo "scale=2; ($rls - $emlp2)/($diff/$seconds)/3600" | bc -lq) hours" &&
+	  [[ $(( $mrls * 2 ))  -gt $rls ]] && echo " (eta likely to climb due to Replica_IO backlog)" || echo
 	[[ $emlp2 -eq $emlp1 ]] && diff=$(( $emlp2 - $emlp1 )) &&
 	  echo "0 $rmlf $emlp2"
-	counter=$((counter+1)) && [[ $counter -gt $seconds && $counter -gt 30 ]] && counter=0
+	counter=$((counter+1)) && [[ $counter -gt $seconds && $counter -gt 10 ]] && counter=0
 done
